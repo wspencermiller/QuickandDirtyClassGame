9e886b5ae08019afe65cdfe1637507bb5dce7ed4
Spencer Miller <wspencer.miller@gmail.com>
2013-04-18 11:33:40 -0500

    InitialProjectCreation

---
+++ ducking-octo-adventure-android/gen/edu/cis4347c/ducking_octo_adventure/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.cis4347c.ducking_octo_adventure;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
