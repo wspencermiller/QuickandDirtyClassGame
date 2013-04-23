b787768b9c2798a8ecbdc39d69d93b478f02304a
Michael Sprague <michael.sprague4@gmail.com>
2013-04-22 19:22:37 -0500

    changed to Paul's version - second time

--- ducking-octo-adventure-android/gen/edu/cis4347c/ducking_octo_adventure/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package edu.cis4347c.ducking_octo_adventure;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
